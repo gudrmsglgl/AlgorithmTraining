4c7983f6340d5762ad3d273db9f84d1fc39b3286
davidHG <gudrmsglgl@gmail.com>
2019-09-16 20:46:35 +0900

    "그리디 알고리즘 ex1"

--- AlgorithmTraining/src/A1.java
+++ AlgorithmTraining/src/A1.java
@@ -1,8 +1,30 @@
 
 public class A1 {
 	
+	// 배낭 채우기
+	// Knapsack = 50w
+	// item1 = ($60, 10w), item2 = ($100, 20w), item3 = ($120, 30w)
+	
 	public static void main(String[] args) {
-		System.out.print("Hello world");
+		
+		int bagMax = 100;
+		
+		int[] itemWeight = {10, 20, 30};
+		int[] itemPrice = {50, 100, 120};
+		
+		int totalPrice = 0;
+		int n = 0;
+		int remain = bagMax;
+		
+		
+		for (int i=3; i>0; i--) {
+			n = remain / itemWeight[i-1];
+			remain = remain % itemWeight[i-1];
+			totalPrice += itemPrice[i-1] * n;
+		}
+		
+		System.out.printf("%s: %d$", "도둑이 훔칠 수 있는 최고 금액" ,totalPrice);
+		
 	}
 	
 }
